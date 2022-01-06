b97613f31c4b9969b6dbc6c5ea3ecc6773342f66
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2022-01-06 15:57:29 +0700

    Create create_config.py

---
+++ src/create_config.py
@@ -0,0 +1,300 @@
+import json
+
+data = {}
+data['mic'] = []
+data['mic'].append({
+    'type': 'None Respeaker Mic',
+    'led_off_mode': '',
+    'led_off_color': '',
+    'led_think_mode': '',    
+    'led_thing_color': '',            
+    'is_active': True           
+})
+data['mic'].append({
+    'type': 'ReSpeaker 2-Mics Pi HAT',
+    'led_off_mode': '',
+    'led_off_color': '',
+    'led_think_mode': '',    
+    'led_thing_color': '',            
+    'is_active': False        
+})
+data['mic'].append({
+    'type': 'ReSpeaker 4-Mics Pi HAT',
+    'led_off_mode': '',
+    'led_off_color': '',
+    'led_think_mode': '',    
+    'led_thing_color': '',            
+    'is_active': False        
+})
+data['mic'].append({
+    'type': 'ReSpeaker Mic Array v2.0',
+    'led_off_mode': 1,
+    'led_off_color': 0xFFFF99,
+    'led_wakeup_mode': 2,
+    'led_wakeup_color': 0x33FFFF,    
+    'is_active': False        
+})
+data['mic'].append({
+    'type': 'ReSpeaker Core v2.0',
+    'led_off_mode': '',
+    'led_off_color': '',
+    'led_think_mode': '',    
+    'led_thing_color': '',            
+    'is_active': False        
+})
+data['volume'] = []
+data['volume'].append({
+    'value': 75,
+    'type': 'event'    
+})
+data['volume'].append({
+    'value': 75,
+    'type': 'speak'    
+})
+data['volume'].append({
+    'value': 75,
+    'type': 'playback'    
+})
+data['hotword_engine'] = []
+data['hotword_engine'].append({
+    'name': 'snowboy',
+    'is_active': False
+})
+data['hotword_engine'].append({
+    'name': 'porcupine',
+    'is_active': True,
+    'porcupine_access_key': 'EhpBuTJẻwrwerwer4IwUtAONg=='
+})
+data['hotword'] = []
+data['hotword'].append({
+    'type': 'snowboy',
+    'file_name': 'snowboy.umdl',    
+    'sensitive': 0.6,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'snowboy',
+    'file_name': 'subex.umdl',    
+    'sensitive': 0.6,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'hey siri_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'americano_raspberry-pi.ppn',    
+    'sensitive': 0.6,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'blueberry_raspberry-pi.ppn',    
+    'sensitive': 0.6,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'terminator_raspberry-pi.ppn',    
+    'sensitive': 0.6,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'ok google_raspberry-pi.ppn',    
+    'sensitive': 0.3,        
+    'is_active': True    
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'hey google_raspberry-pi.ppn',    
+    'sensitive': 0.6,        
+    'is_active': True   
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'alexa_raspberry-pi.ppn',    
+    'sensitive': 0.6,        
+    'is_active': True   
+})
+data['hotword'].append({
+    'type': 'porcupine',
+    'file_name': 'jarvis_raspberry-pi.ppn',    
+    'sensitive': 0.6,        
+    'is_active': True   
+})
+data['continuous_asking'] = []
+data['continuous_asking'].append({
+    'content': 'hỏi liên tục',
+    'is_active': False
+})
+data['stt_engine'] = []
+data['stt_engine'].append({
+    'name': 'stt_gg_free',
+    'time_out': 6000,
+    'is_active': False    
+})
+data['stt_engine'].append({
+    'name': 'stt_gg_cloud',
+    'token_file': 'google.json',    
+    'time_out': 6000,
+    'is_active': True    
+})
+data['stt_engine'].append({
+    'name': 'stt_gg_ass',
+    'token': '',
+    'token_file': '',    
+    'time_out': 6000,
+    'is_active': False    
+})
+data['stt_engine'].append({
+    'name': 'stt_viettel',
+    'token': 'SythBY7N8AUsdfsdfdsfpxlyXxzdWRNwYE8N',
+    'time_out': 4000,
+    'is_active': False    
+})
+data['stt_engine'].append({
+    'name': 'stt_fpt',
+    'token': '',
+    'token_file': '',    
+    'time_out': '',
+    'is_active': False    
+})
+data['local_tts'] = []
+data['local_tts'].append({
+    'name': 'tts_gg_free',
+    'token': '8sJJsdfsdfsdfBXC2fRGU',    
+    'voice_name': '',    
+    'speed': '',
+    'pitch': '',
+    'is_active': False    
+})
+data['local_tts'].append({
+    'token': 'AIzaSysdfsdfsdfk16b3c',
+    'token_file': 'google.json',    
+    'name': 'tts_gg_cloud',    
+    'voice_name': 'vi-VN-Wavenet-A',
+    'profile': 'telephony-class-application',      
+    'speed': 1.0,
+    'pitch': 0,
+    'is_active': False    
+})
+data['local_tts'].append({
+    'token': 'SythBY7NsfsdfsdfdWRNwYE8N',
+    'token_file': '',    
+    'name': 'tts_viettel',    
+    'voice_name': 'hcm-diemmy2',
+    'speed': 1.0,
+    'pitch': '',
+    'is_active': False    
+})
+data['local_tts'].append({
+    'token': '8sJJ3sfsdfsdXC2fRGU',
+    'token_file': '',    
+    'name': 'tts_zalo',
+    'voice_name': '1',    
+    'speed': 1.0,
+    'pitch': '',
+    'is_active': False    
+})    
+data['local_tts'].append({
+    'token': '9onFCsfsdfjjDU',
+    'name': 'tts_fpt',
+    'voice_name': 'ngoclam',
+    'speed': 1.0,
+    'pitch': '',    
+    'is_active': False    
+})
+data['remote_tts'] = []
+data['remote_tts'].append({
+    'voice_engine': 'tts_fpt',
+    'voice_name': 'ngoclam',
+    'voice_speed': 1.0,
+    'is_active': True    
+})
+data['remote_tts'].append({
+    'voice_engine': 'tts_zalo',
+    'voice_name': '1',
+    'voice_speed': 1.0,
+    'is_active': False    
+})
+
+data['hass_skill'] = []
+data['hass_skill'].append({
+    'hass_url':'https://test123.duckdns.org',
+    'hass_token': '123sfsdfjlsfj',
+    'is_active': True        
+})
+
+data['incrase_volume'] = []
+data['incrase_volume'].append({
+    'content': 'to lên',
+    'is_active': True
+})
+data['incrase_volume'].append({
+    'content': 'to thêm',
+    'is_active': True
+})
+data['incrase_volume'].append({
+    'content': 'tăng âm lượng',
+    'is_active': True
+})
+data['decrase_volume'] = []
+data['decrase_volume'].append({
+    'content': 'bé xuống',
+    'is_active': True
+})
+data['decrase_volume'].append({
+    'content': 'bé đi',
+    'is_active': True
+})
+data['decrase_volume'].append({
+    'content': 'giảm âm lượng',
+    'is_active': True
+})
+data['pause'] = []
+data['pause'].append({
+    'content': 'tạm dừng',
+    'is_active': True
+})
+data['continue'] = []
+data['continue'].append({
+    'content': 'tiếp tục',
+    'is_active': True
+})
+data['reply'] = []
+data['reply'].append({
+    'content': 'phát lại',
+    'is_active': True
+})
+data['reply'].append({
+    'content': 'bật lại',
+    'is_active': True
+})
+data['next'] = []
+data['reply'].append({
+    'content': 'kế tiếp',
+    'is_active': True
+})
+data['reply'].append({
+    'content': 'tiếp theo',
+    'is_active': True
+})
+data['exit'] = []
+data['exit'].append({
+    'content': 'exit',
+    'is_active': True
+})
+data['exit'].append({
+    'content': 'thoát',
+    'is_active': True
+})
+data['exit'].append({
+    'content': 'stop',
+    'is_active': True
+})
+with open('config.json', 'w') as outfile:
+    json.dump(data, outfile)
